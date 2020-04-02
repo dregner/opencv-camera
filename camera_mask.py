f7c0f19ae5d927f1c063bda58e02988b0231317d
daniel <daniel_regner@hotmail.com>
2020-04-01 22:00:00 -0300

    Updating code

--- camera_mask.py
+++ camera_mask.py
@@ -25,10 +25,23 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+#Function to increase brighness
+def adjust_gamma(image, gamma=1.0):
+
+   invGamma = 1.0 / gamma
+   table = np.array([((i / 255.0) ** invGamma) * 255
+      for i in np.arange(0, 256)]).astype("uint8")
+
+   return cv2.LUT(image, table)
+
 while(1):
 
     # Take each frame
     _, frame = cap.read()
+
+    #iluminate image
+    gamma = 1.5
+    frame = adjust_gamma(frame, gamma=gamma)
 
     # Convert BGR to HSV
     # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -54,15 +67,21 @@ while(1):
     # mask_r = cv2.inRange(hsv, lower_red, upper_red)
     mask_p = cv2.inRange(hsv, lower_purple, upper_purple)
 
-    mask_t = mask_g | mask_b | mask_p
-    mask_t = cv2.erode(mask_t, None, iterations=5)
-    mask_t = cv2.dilate(mask_t, None, iterations=5)
+    mask_t = mask_g | mask_p
+    kernel = np.ones((10,10), np.uint8)
+    mask_t = cv2.erode(mask_t, kernel, iterations=1)
+    mask_t = cv2.dilate(mask_t, kernel, iterations=1)
 
-    
+    # ret, threshol = cv2.adaptiveThreshold(mask_t, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # ret, threshol = cv2.threshold(gray,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # ret, threshol = cv2.threshold(gray,10, 255,cv2.THRESH_OTSU)
+
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask_t)
 
-    # cv2.imshow('frame',gaussian_blur)
+    cv2.imshow('frame',frame)
+    # cv2.imshow('threshold', threshol)
     cv2.imshow('mask', mask_t)
     cv2.imshow('res',res)
 
@@ -72,8 +91,10 @@ while(1):
 
 cv2.destroyAllWindows()
 
-# masks
+    # masks determine value for color in HSV
+# $ python
+# >>> import cv2
+# >>> import numpy as np  
 # >>> green = np.uint8([[[0,255,0 ]]])
 # >>> hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
 # >>> print hsv_green
-# [[[ 60 255 255]]]
